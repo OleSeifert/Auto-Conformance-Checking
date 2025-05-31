6a923c42f70f699f8e322d7d1b5f5b47861b5ec5
rene <r.rockstedt@googlemail.com>
2025-05-31 15:54:10 +0200

    Add log-skeleton queries

--- backend/pql_queries/general_queries.py
+++ backend/pql_queries/general_queries.py
@@ -24,6 +24,19 @@ def get_dfg_representation(celonis: CelonisConnectionManager) -> DataFrame:
     return celonis.get_dataframe_from_celonis(dfg_query)  # type: ignore
 
 
+def get_cases(celonis: CelonisConnectionManager) -> DataFrame:
+    """A query that gets the cases from an event log.
+
+    Args:
+        celonis (CelonisConnectionManager): the celonis connection
+
+    Returns:
+        a pandas Dataframe that contains the cases
+    """
+    case_query = {"Case Count": """ DISTINCT "ACTIVITIES"."case:concept:name" """}
+    return celonis.get_dataframe_from_celonis(case_query)  # type: ignore
+
+
 def get_number_of_cases(celonis: CelonisConnectionManager) -> DataFrame:
     """A query that gets the count of cases from an event log.
 
@@ -39,8 +52,8 @@ def get_number_of_cases(celonis: CelonisConnectionManager) -> DataFrame:
     return celonis.get_dataframe_from_celonis(case_query)  # type: ignore
 
 
-def get_number_of_activities(celonis: CelonisConnectionManager) -> DataFrame:
-    """A query that gets the count of activities from an event log.
+def get_activities(celonis: CelonisConnectionManager) -> DataFrame:
+    """A query that gets the activities from an event log.
 
     Args:
         celonis (CelonisConnectionManager): the celonis connection
@@ -48,7 +61,36 @@ def get_number_of_activities(celonis: CelonisConnectionManager) -> DataFrame:
     Returns:
         a pandas Dataframe that contains the count of cases
     """
+    activity_query = {"Activities": """ DISTINCT "ACTIVITIES"."concept:name" """}
+    return celonis.get_dataframe_from_celonis(activity_query)  # type: ignore
+
+
+def get_number_of_activities(celonis: CelonisConnectionManager) -> DataFrame:
+    """A query that gets the count of activities from an event log.
+
+    Args:
+        celonis (CelonisConnectionManager): the celonis connection
+
+    Returns:
+        a pandas Dataframe that contains the count of activites
+    """
     activity_query = {
         "Activity Count": """ COUNT (DISTINCT "ACTIVITIES"."concept:name" )"""
     }
     return celonis.get_dataframe_from_celonis(activity_query)  # type: ignore
+
+
+def get_traces_with_count(celonis: CelonisConnectionManager) -> DataFrame:
+    """A query that gets the traces and their count.
+
+    Args:
+        celonis (CelonisConnectionManager): the celonis connection
+
+    Returns:
+        a pandas Dataframe that contains the traces and their count
+    """
+    trace_query = {
+        "Trace": """DISTINCT VARIANT ("ACTIVITIES"."concept:name")""",
+        "Count": """COUNT (VARIANT ("ACTIVITIES"."concept:name"))""",
+    }
+    return celonis.get_dataframe_from_celonis(trace_query)  # type: ignore
